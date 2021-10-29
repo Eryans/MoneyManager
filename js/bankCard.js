f08fe9fd5e4ad515a54ea341977a102074011a7e
Julio <julesnoir@hotmail.fr>
2021-10-29 11:01:01 +0200

    made a class to create bank account Card, scrapped the expandable form idea as it is too much work right now

--- js/bankCard.js
+++ js/bankCard.js
@@ -1,30 +1,68 @@
-// Card cloning testing
-const bankCard = document.querySelector(".bankCard");
-let expandBtns = document.getElementsByClassName("bankExpandButton");
 
-// TODO bankCard object
+// BankCard object
 class BankCard{
-    constructor(){
-        this.card = bankCard.cloneNode(true);
+    constructor(money,accountNumber,expiration,ccCode){
+        this.money = money;
+        this.accountNumber = accountNumber;
+        this.expiration = expiration;
+        this.ccCode = ccCode;
+    }
+    create(parentNode){
+        const CARD = document.createElement("article");
+        CARD.classList.add("bg-light", "col-10", "my-2", "p-2", "shadow", "bankCard" ,"d-flex" ,"flex-column")
+        CARD.innerHTML = `  
+        <div class=" bg-primary text-white shadow p-2 d-flex flex-column">
+          <table class="d-block">
+            <tr>
+              <th>
+                solde :
+              </th>
+            </tr>
+            <tr>
+              <td class="balanceTotal">
+                ${this.money} €
+              </td>
+            </tr>
+            <tr>
+              <th>
+                Numéro Carte :
+              </th>
+            </tr>
+            <tr>
+              <td class="cardNumber">
+                ${this.accountNumber}
+              </td>
+            </tr>
+            <tr>
+              <th>
+                Expiration :
+              </th>
+              <th>
+                CCV
+              </th>
+            </tr>
+            <tr>
+              <td class="expiration">
+                ${this.expiration}
+              </td>
+              <td class="cccode">
+                ${this.ccCode}
+              </td>
+            </tr>
+          </table>
+        </div>`
+        parentNode.appendChild(CARD);
+    }
+    expand(){
+
     }
 }
 
-
-
-function createNewCard(money,accountNumber,expiration,ccCode){
-    newCard = new BankCard();
-    document.querySelector("#bankAccount").appendChild(newCard.card);
-}
-
-createNewCard();
-
-Array.from(expandBtns).forEach(x => x.addEventListener("click",function(){
-    this.style.color = "red";
-    let options = this.nextElementSibling;
-    options.classList.toggle("reduced");
-}));
-
-createNewCard();
-
-
-console.log(expandBtns);
+// Testing
+const CARDS = [];
+for (let i = 0; i < 3; i++)
+{
+    let newCard = new BankCard(250,"145 230 540 FR 20","12/25","123");
+    CARDS.push(newCard)
+;}
+CARDS.forEach(x => x.create(document.querySelector("main")));
