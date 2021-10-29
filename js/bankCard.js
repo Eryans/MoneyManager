5f571a69964faca4d525f2b9117a15201577e047
Julio <julesnoir@hotmail.fr>
2021-10-29 11:23:35 +0200

    BankCard now generate random numbers

--- js/bankCard.js
+++ js/bankCard.js
@@ -57,12 +57,16 @@ class BankCard{
 
     }
 }
-
+let defaultCard = new BankCard(250,"145 230 540 FR 20","12/25","123");
+defaultCard.create(document.querySelector("#bankAccount"));
 // Testing
 const CARDS = [];
-for (let i = 0; i < 3; i++)
+function createCard()
 {
-    let newCard = new BankCard(250,"145 230 540 FR 20","12/25","123");
-    CARDS.push(newCard)
-;}
-CARDS.forEach(x => x.create(document.querySelector("main")));
+    let newCard = new BankCard(0,`${randomRange(1000,9999)} ${randomRange(1000,9999)} ${randomRange(1000,9999)} FR ${randomRange(10,99)}`,`${randomRange(1,12)}/26`,randomRange(100,999));
+    newCard.create(document.querySelector("#bankAccount"));
+    CARDS.push(newCard);
+}
+function randomRange(min,max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
