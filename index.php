07af6e6f5b3080cfa1a84e4fe12311fd5b0addbd
Julio <julesnoir@hotmail.fr>
2021-12-05 13:31:42 +0100

    blocker is now JS generated

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<div id="blocker" class="bg-dark text-white position-fixed h-100 w-100"></div>
-  <section id="securityInfo" class="bg-light position-fixed container p-0">
-    <h2 id = "securityTitle" class="bg-primary col-12 p-2 m-0 ">chargement.</h2>
-    <p id = "securityText" class="m-2"> chargement.</p>
-    <button id="securityBtn"class="m-2">J'ai compris</button>
-  </section>
 
 
 <?php include "./layout/header.php" ?>
--- js/blocker.js
+++ js/blocker.js
@@ -1,7 +1,25 @@
-
 // -------------------------- Blocker ----------------------------------- //
 
+let blocker = document.createElement("div");
+blocker.id = "blocker";
+blocker.classList.add("bg-dark" ,"text-white", "position-fixed" ,"h-100" ,"w-100","top-0","start-0");
+
+let content = document.createElement("div");
+content.innerHTML = `
+  <section id="securityInfo" class="bg-light position-fixed container p-0">
+    <h2 id = "securityTitle" class="bg-primary col-12 p-2 m-0 ">chargement.</h2>
+    <p id = "securityText" class="m-2"> chargement.</p>
+    <button id="securityBtn"class="m-2">J'ai compris</button>
+  </section> `;
+
+sessionStorage.setItem("hasBeenAccepted",false);
+let hasBeenAccepted = sessionStorage.getItem("hasBeenAccepted");
+
+if (true){
+document.body.appendChild(blocker);
+document.body.appendChild(content);
 document.querySelector("#securityBtn").addEventListener("click",function(){
+    sessionStorage.setItem("hasBeenAccepted",true);
     document.querySelector("#blocker").remove();
     document.querySelector("#securityInfo").remove();
 })
@@ -23,4 +41,4 @@ httpRequest.onreadystatechange = function(){
 }
 httpRequest.open("GET","json/security.json",true);
 httpRequest.send();
-// 
+}
